cc1fb3b9673878e03c50b63ee11824ea8653577a
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2023-05-31 22:54:33 -0300

    35

--- comprar-tickets.js
+++ comprar-tickets.js
@@ -1,9 +1,6 @@
 // variables globales
 
-const cards = document.querySelectorAll(".card");
-let nombre = document.querySelector("#nombre"),
-apellido = document.querySelector("#apellido"),
-correo = document.querySelector("#correo"),
+let formulario = document.querySelector(".form"),
 cantidad = document.querySelector("#cantidad"),
 categoria = document.querySelector("#categoria"),
 total = document.querySelector(".total"),
@@ -11,3 +8,26 @@ borrar = document.querySelector(".borrar"),
 resumen = document.querySelector(".resumen");
 
 
+resumen.addEventListener("click", (e) => {
+
+    total.value = "Total a pagar: $";
+
+    e.preventDefault();
+
+    let valorCategoria = 0;
+
+    if (categoria.value == "estudiante") {
+        valorCategoria = (cantidad.value * 200 ) * 80 / 100;
+    } else if (categoria.value == "trainee"){
+        valorCategoria = (cantidad.value * 200 ) * 50 / 100;
+    } else if (categoria.value == "junior"){
+        valorCategoria = (cantidad.value * 200 ) * 15 / 100;
+    }
+
+    total.value += ` ${valorCategoria}`;
+});
+
+borrar.addEventListener("click" , (e) => {
+    e.preventDefault();
+    formulario.reset();
+});
